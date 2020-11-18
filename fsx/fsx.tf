84766319a20cf14ee90180e9971011f6ee957163
Chris Kinsella <chris.kinsella@helkinsoft.com>
2020-11-18 14:51:54 +0000

    Added Route53 records to allow clients to get IPs for FSX

--- fsx/fsx.tf
+++ fsx/fsx.tf
@@ -1,6 +1,7 @@
 # https://registry.terraform.io/providers/hashicorp/aws/2.70.0/docs/resources/fsx_windows_file_system
 
 resource "aws_fsx_windows_file_system" "mis_bfs" {
+
   active_directory_id               = data.terraform_remote_state.activedirectory.outputs.mis_ad["id"]
   storage_capacity                  = 300
   throughput_capacity               = 64 # MB/Second in power of 2 increments. Minimum of 8 and maximum of 2048.
@@ -33,11 +34,3 @@ resource "aws_fsx_windows_file_system" "mis_bfs" {
 
 }
 
-# ndl-bfs-101-fsx.delius-mis-dev.internal
-resource "aws_route53_record" "aws_fsx_windows_file_system" {
-  zone_id = local.public_zone_id
-  name    = "${local.bfs_filesystem_name}.${local.domain_name}"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [aws_fsx_windows_file_system.mis_bfs.dns_name]
-}
