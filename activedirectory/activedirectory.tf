e2cd0b2d06946da472fe56d1e7c503a586052bc4
Chris Kinsella <chris.kinsella@helkinsoft.com>
2021-03-05 17:19:12 +0000

    Added AD Lifecycle rule to prevent TF trying to destroy/create AD on every plan

--- activedirectory/activedirectory.tf
+++ activedirectory/activedirectory.tf
@@ -2,8 +2,8 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/directory_service_directory
 
 resource "aws_directory_service_directory" "mis_ad" {
-  name        = "${local.environment_name}.local"    # delius-mis-dev.local
-  short_name  = local.environment_name               # delius-mis-dev
+  name        = "${local.environment_name}.local"    # ie. delius-mis-dev.local
+  short_name  = local.environment_name               # ie. delius-mis-dev
   description = "Microsoft AD for ${local.environment_name}.local"
   password    = local.ad_admin_password
   enable_sso  = false
@@ -19,7 +19,16 @@ resource "aws_directory_service_directory" "mis_ad" {
   tags = merge(
     local.tags,
     {
-      "Name" = local.environment_name
+      "Name" = data.aws_ssm_parameter.ad_admin_password.value
     },
   )
+
+  # Required as AWS does not allow you to change the Admin password post AD Create - you must destroy/recreate 
+  # When we run tf plan against an already created AD it will always show the AD needs destroy/create so we ignore this change
+  lifecycle {
+    ignore_changes = [
+      password  
+    ]
+  }
+
 }
