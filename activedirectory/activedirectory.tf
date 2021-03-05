20004849c3628d493ed3391ac228af0a4364f1f4
Chris Kinsella <chris.kinsella@helkinsoft.com>
2021-03-05 17:21:47 +0000

    set AD to get password from SSM ParamStore

--- activedirectory/activedirectory.tf
+++ activedirectory/activedirectory.tf
@@ -5,7 +5,7 @@ resource "aws_directory_service_directory" "mis_ad" {
   name        = "${local.environment_name}.local"    # ie. delius-mis-dev.local
   short_name  = local.environment_name               # ie. delius-mis-dev
   description = "Microsoft AD for ${local.environment_name}.local"
-  password    = local.ad_admin_password
+  password    = data.aws_ssm_parameter.ad_admin_password.value
   enable_sso  = false
   type        = "MicrosoftAD"
   edition     = "Standard"
@@ -19,12 +19,12 @@ resource "aws_directory_service_directory" "mis_ad" {
   tags = merge(
     local.tags,
     {
-      "Name" = data.aws_ssm_parameter.ad_admin_password.value
+      "Name" = local.environment_name
     },
   )
 
   # Required as AWS does not allow you to change the Admin password post AD Create - you must destroy/recreate 
-  # When we run tf plan against an already created AD it will always show the AD needs destroy/create so we ignore this change
+  # When we run tf plan against an already created AD it will always show the AD needs destroy/create so we ignore
   lifecycle {
     ignore_changes = [
       password  
