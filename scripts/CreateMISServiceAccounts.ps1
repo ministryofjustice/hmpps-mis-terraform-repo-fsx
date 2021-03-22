f1861ffc551c3df02c6f741624dbd310a49bb0f0
Chris Kinsella <chris.kinsella@helkinsoft.com>
2021-03-22 09:53:42 +0000

    fixed create AD service accounts script

--- scripts/CreateMISServiceAccounts.ps1
+++ scripts/CreateMISServiceAccounts.ps1
@@ -39,7 +39,7 @@ write-output '==================================================================
 $svc_username_SSMPath = "/" + $environmentName.Value + "/" + $application.Value + "/mis-service-accounts/SVC_BOSSO-NDL/SVC_BOSSO-NDL_username"
 $svc_password_SSMPath = "/" + $environmentName.Value + "/" + $application.Value + "/mis-service-accounts/SVC_BOSSO-NDL/SVC_BOSSO-NDL_password"
 
-Write-Output "Getting the Service Username from ${svc_username_SSMPath}"    
+Write-Output "Getting the Service Username from ${svc_username_SSMPath}"
 $svc_username = Get-SSMParameter -Name $svc_username_SSMPath -WithDecryption $true
 
 Write-Output "Getting the Service Password from ${svc_password_SSMPath}"
@@ -49,7 +49,10 @@ $ServiceUsername = $svc_username.Value
 
 # netbios names limit of 15 chars so delius-auto-test has to be truncated to delius-auto-tes
 # other env names are under 15 chars so not truncated
-$domainname = $domainname.SubString(0,15)
+if($domainname.Length -gt 15) {
+    $domainname = $domainname.SubString(0,15)
+}
+
 $OUPath = "OU=Users,OU=${domainname},DC=${domainname},DC=local"
 
 Write-Output "Creating the user ${ServiceUsername} in '${OUPath}'"
@@ -57,7 +60,7 @@ $SecureAccountPassword = $svc_password.Value | ConvertTo-SecureString -AsPlainTe
 New-ADUser -Name $ServiceUsername -GivenName $ServiceUsername -Surname "" -Path $OUPath -AccountPassword $SecureAccountPassword -Enabled $true
 
 Write-Output "Checking the AD for the newly created user ${ServiceUsername}"
-Get-ADUser -Filter * -Properties samAccountName | Where { $_.samAccountName -eq $ServiceUsername } | select samAccountName, DistinguishedName
+Get-ADUser -Filter * -Properties SamAccountName | Where { $_.samAccountName -eq $ServiceUsername }
 
 $ServiceUsername = ""
 
@@ -68,7 +71,7 @@ write-output '==================================================================
 $svc_username_SSMPath = "/" + $environmentName.Value + "/" + $application.Value + "/mis-service-accounts/SVC_DS_AD_DEV/SVC_DS_AD_DEV_username"
 $svc_password_SSMPath = "/" + $environmentName.Value + "/" + $application.Value + "/mis-service-accounts/SVC_DS_AD_DEV/SVC_DS_AD_DEV_password"
 
-Write-Output "Getting the Service Username from ${svc_username_SSMPath}"    
+Write-Output "Getting the Service Username from ${svc_username_SSMPath}"
 $svc_username = Get-SSMParameter -Name $svc_username_SSMPath -WithDecryption $true
 
 Write-Output "Getting the Service Password from ${svc_password_SSMPath}"
@@ -76,10 +79,43 @@ $svc_password = Get-SSMParameter -Name $svc_password_SSMPath -WithDecryption $tr
 
 $ServiceUsername = $svc_username.Value
 
-$OUPath = "OU=Users,OU=delius-mis-dev,DC=delius-mis-dev,DC=local"
+$OUPath = "OU=Users,OU=${domainname},DC=${domainname},DC=local"
+Write-Output "Creating the user ${ServiceUsername} in '${OUPath}'"
+$SecureAccountPassword = $svc_password.Value | ConvertTo-SecureString -AsPlainText -Force
+New-ADUser -Name $ServiceUsername -GivenName $ServiceUsername -Surname "" -Path $OUPath -AccountPassword $SecureAccountPassword -Enabled $true
+
+
+Write-Output "Checking the AD for the newly created user ${ServiceUsername}"
+Get-ADUser -Filter * -Properties SamAccountName | Where { $_.samAccountName -eq $ServiceUsername }
+
+$ServiceUsername = ""
+
+write-output '================================================================================'
+write-output " Creating Service Account SVC_DFI_NDL"
+write-output '================================================================================'
+
+$svc_username_SSMPath = "/" + $environmentName.Value + "/" + $application.Value + "/mis-service-accounts/SVC_DFI_NDL/SVC_DFI_NDL_username"
+$svc_password_SSMPath = "/" + $environmentName.Value + "/" + $application.Value + "/mis-service-accounts/SVC_DFI_NDL/SVC_DFI_NDL_password"
+
+Write-Output "Getting the Service Username from ${svc_username_SSMPath}"
+$svc_username = Get-SSMParameter -Name $svc_username_SSMPath -WithDecryption $true
+
+Write-Output "Getting the Service Password from ${svc_password_SSMPath}"
+$svc_password = Get-SSMParameter -Name $svc_password_SSMPath -WithDecryption $true
+
+$ServiceUsername = $svc_username.Value
+
+# netbios names limit of 15 chars so delius-auto-test has to be truncated to delius-auto-tes
+# other env names are under 15 chars so not truncated
+if($domainname.Length -gt 15) {
+    $domainname = $domainname.SubString(0,15)
+}
+
+$OUPath = "OU=Users,OU=${domainname},DC=${domainname},DC=local"
+
 Write-Output "Creating the user ${ServiceUsername} in '${OUPath}'"
 $SecureAccountPassword = $svc_password.Value | ConvertTo-SecureString -AsPlainText -Force
 New-ADUser -Name $ServiceUsername -GivenName $ServiceUsername -Surname "" -Path $OUPath -AccountPassword $SecureAccountPassword -Enabled $true
 
 Write-Output "Checking the AD for the newly created user ${ServiceUsername}"
-Get-ADUser -Filter * -Properties samAccountName | Where { $_.samAccountName -eq $ServiceUsername } | select samAccountName, DistinguishedName
+Get-ADUser -Filter * -Properties samAccountName | Where { $_.samAccountName -eq $ServiceUsername }
